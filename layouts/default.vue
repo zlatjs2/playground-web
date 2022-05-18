963fed1d7a152ebe66915c7e1f63f95b6bc1911a
김선미 <zlatjs2@gmail.com>
2022-05-18 14:25:21 +0900

    feat: 디폴트 레이아웃 추가

---
+++ layouts/default.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="app-wrapper">
+    <main class="app-container">
+      <Nuxt />
+    </main>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DefaultLayout',
+}
+</script>
+
+<style></style>
